7eb5ad68ca46fa88fc81144ab94a033fb52028f8
Jun Kyung Lee <junlee0325@gmail.com>
2024-11-14 03:17:04 -0500

    client-picked albums updated

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -6,60 +6,210 @@ export const Gallery = () => {
   const { t } = useTranslation();
 
     
-    const albumA = [
-      "/images/img1.jpg",
-      "/images/img2.jpg",
-      "/images/img3.jpg",
-      "/images/img5.jpg",
-      "/images/img4.jpg"
+    const album01 = [
+      "/images/a01/p1.jpg",
+      "/images/a01/p2.jpg",
+      "/images/a01/p3.jpg",
+      "/images/a01/p4.jpg"
     ]
 
-    const albumB = [
-      "/images/img6.jpg",
-      "/images/img7.jpg",
-      "/images/img8.jpg",
-      "/images/img9.jpg",
-      "/images/img10.jpg",
-      "/images/img11.jpg"
+    const album02 = [
+      "/images/a02/p1.jpg",
+      "/images/a02/p2.jpg",
+      "/images/a02/p3.jpg",
+      "/images/a02/p4.jpg",
+      "/images/a02/p5.jpg",
+      "/images/a02/p6.jpg",
+      "/images/a02/p7.jpg",
+      "/images/a02/p8.jpg"
     ]
 
-    const albumC = [
-      "/images/img12.jpg",
-      "/images/img13.jpg",
-      "/images/img14.jpg",
-      "/images/img15.jpg"
+    const album03 = [
+      "/images/a03/p1.jpg",
+      "/images/a03/p2.jpg",
+      "/images/a03/p3.jpg",
+      "/images/a03/p4.jpg"
     ]
 
-    const albumD = [
-      "/images/img16.jpg",
-      "/images/img17.jpg"
+    const album04 = [
+      "/images/a04/p1.jpg",
+      "/images/a04/p2.jpg",
+      "/images/a04/p3.jpg",
+      "/images/a04/p4.jpg",
+      "/images/a04/p5.jpg",
+      "/images/a04/p6.jpg",
+      "/images/a04/p7.jpg",
+      "/images/a04/p8.jpg"
     ]
 
-    const albumE = [
-      "/images/img18.jpg",
-      "/images/img19.jpg",
-      "/images/img20.jpg"
+    const album05 = [
+      "/images/a05/p1.jpg",
+      "/images/a05/p2.jpg",
+      "/images/a05/p3.jpg",
+      "/images/a05/p4.jpg",
+      "/images/a05/p5.jpg",
+      "/images/a05/p6.jpg",
+      "/images/a05/p7.jpg"
     ]
 
-    const albumF = [
-      "/images/img21.jpg",
-      "/images/img22.jpg"
+    const album06 = [
+      "/images/a06/p1.jpg",
+      "/images/a06/p2.jpg",
+      "/images/a06/p3.jpg",
+      "/images/a06/p4.jpg",
+      "/images/a06/p5.jpg",
+      "/images/a06/p6.jpg",
+      "/images/a06/p7.jpg",
+      "/images/a06/p8.jpg",
+      "/images/a06/p9.jpg",
+      "/images/a06/p10.jpg"
     ]
 
-    const albumG = [
-      "/images/img23.jpg",
-      "/images/img24.jpg"
+    const album07 = [
+      "/images/a07/p1.jpg",
+      "/images/a07/p2.jpg",
+      "/images/a07/p3.jpg",
+      "/images/a07/p4.jpg",
+      "/images/a07/p5.jpg",
+      "/images/a07/p6.jpg",
+      "/images/a07/p7.jpg"
     ]
 
-    const albumH = [
-      "/images/img25.jpg",
-      "/images/img26.jpg"
+    const album08 = [
+      "/images/a08/p1.jpg",
+      "/images/a08/p2.jpg",
+      "/images/a08/p3.jpg",
+      "/images/a08/p4.jpg",
+      "/images/a08/p5.jpg",
+      "/images/a08/p6.jpg"
     ]
 
-    const albumI = [
-      "/images/img27.jpg",
-      "/images/img28.jpg",
-      "/images/img29.jpg"
+    const album09 = [
+      "/images/a09/p1.jpg",
+      "/images/a09/p2.jpg",
+      "/images/a09/p3.jpg",
+      "/images/a09/p4.jpg",
+      "/images/a09/p5.jpg",
+      "/images/a09/p6.jpg",
+      "/images/a09/p7.jpg",
+      "/images/a09/p8.jpg"
+    ]
+
+    const album10 = [
+      "/images/a10/p1.jpg",
+      "/images/a10/p2.jpg",
+      "/images/a10/p3.jpg",
+      "/images/a10/p4.jpg"
+    ]
+
+    const album11 = [
+      "/images/a11/p1.jpg",
+      "/images/a11/p2.jpg",
+      "/images/a11/p3.jpg",
+      "/images/a11/p4.jpg",
+      "/images/a11/p5.jpg"
+    ]
+
+    const album12 = [
+      "/images/a12/p1.jpg",
+      "/images/a12/p2.jpg",
+      "/images/a12/p3.jpg",
+      "/images/a12/p4.jpg",
+      "/images/a12/p5.jpg",
+      "/images/a12/p6.jpg",
+      "/images/a12/p7.jpg"
+    ]
+
+    const album13 = [
+      "/images/a13/p1.jpg",
+      "/images/a13/p2.jpg",
+      "/images/a13/p3.jpg",
+      "/images/a13/p4.jpg",
+      "/images/a13/p5.jpg",
+      "/images/a13/p6.jpg",
+      "/images/a13/p7.jpg",
+      "/images/a13/p8.jpg",
+      "/images/a13/p9.jpg",
+      "/images/a13/p10.jpg",
+      "/images/a13/p11.jpg"
+    ]
+
+    const album14 = [
+      "/images/a14/p1.jpg",
+      "/images/a14/p2.jpg",
+      "/images/a14/p3.jpg",
+      "/images/a14/p4.jpg",
+      "/images/a14/p5.jpg",
+      "/images/a14/p6.jpg"
+    ]
+
+    const album15 = [
+      "/images/a15/p1.jpg",
+      "/images/a15/p2.jpg",
+      "/images/a15/p3.jpg",
+      "/images/a15/p4.jpg",
+      "/images/a15/p5.jpg",
+      "/images/a15/p6.jpg",
+      "/images/a15/p7.jpg",
+      "/images/a15/p8.jpg",
+      "/images/a15/p9.jpg",
+      "/images/a15/p10.jpg",
+      "/images/a15/p11.jpg",
+      "/images/a15/p12.jpg",
+      "/images/a15/p13.jpg",
+      "/images/a15/p14.jpg",
+      "/images/a15/p15.jpg",
+      "/images/a15/p16.jpg",
+      "/images/a15/p17.jpg",
+      "/images/a15/p18.jpg"
+    ]
+
+    const album16 = [
+      "/images/a16/p1.jpg",
+      "/images/a16/p2.jpg",
+      "/images/a16/p3.jpg",
+      "/images/a16/p4.jpg",
+      "/images/a16/p5.jpg",
+      "/images/a16/p6.jpg",
+      "/images/a16/p7.jpg",
+      "/images/a16/p8.jpg",
+      "/images/a16/p9.jpg",
+      "/images/a16/p10.jpg",
+      "/images/a16/p11.jpg",
+      "/images/a16/p12.jpg",
+      "/images/a16/p13.jpg",
+      "/images/a16/p14.jpg",
+      "/images/a16/p15.jpg",
+      "/images/a16/p16.jpg",
+      "/images/a16/p17.jpg",
+      "/images/a16/p18.jpg",
+      "/images/a16/p19.jpg"
+    ]
+
+    const album17 = [
+      "/images/a17/p1.jpg",
+      "/images/a17/p2.jpg",
+      "/images/a17/p3.jpg"
+    ]
+
+    const album18 = [
+      "/images/a18/p1.jpg",
+      "/images/a18/p2.jpg"
+    ]
+
+    const album19 = [
+      "/images/a19/p1.jpg",
+      "/images/a19/p2.jpg",
+      "/images/a19/p3.jpg",
+      "/images/a19/p4.jpg"
+    ]
+
+    const album20 = [
+      "/images/a20/p1.jpg",
+      "/images/a20/p2.jpg",
+      "/images/a20/p3.jpg",
+      "/images/a20/p4.jpg",
+      "/images/a20/p5.jpg"
     ]
 
   return (
@@ -67,15 +217,26 @@ export const Gallery = () => {
         <h2>{t("gallery")}</h2>
 
         <div className="albums">
-          <Album albumID={albumA} />
-          <Album albumID={albumB} />
-          <Album albumID={albumC} />
-          <Album albumID={albumD} />
-          <Album albumID={albumE} />
-          <Album albumID={albumF} />
-          <Album albumID={albumG} />
-          <Album albumID={albumH} />
-          <Album albumID={albumI} />
+          <Album albumID={album01} />
+          <Album albumID={album02} />
+          <Album albumID={album03} />
+          <Album albumID={album04} />
+          <Album albumID={album05} />
+          <Album albumID={album06} />
+          <Album albumID={album07} />
+          <Album albumID={album08} />
+          <Album albumID={album09} />
+          <Album albumID={album10} />
+          <Album albumID={album11} />
+          <Album albumID={album12} />
+          <Album albumID={album13} />
+          <Album albumID={album14} />
+          <Album albumID={album15} />
+          <Album albumID={album16} />
+          <Album albumID={album17} />
+          <Album albumID={album18} />
+          <Album albumID={album19} />
+          <Album albumID={album20} />
         </div>
     </div>
   )
