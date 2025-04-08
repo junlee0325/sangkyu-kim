c5e58782ca1c9d658de0612abb69c7ba56c38dc0
Jun Kyung Lee <junlee0325@gmail.com>
2025-04-08 04:00:37 -0400

    Update App.js
    added footer

--- src/App.js
+++ src/App.js
@@ -17,6 +17,15 @@ function App() {
         <Gallery />
         <Contact />
       </div>
+    <footer>
+		<span>© 2024 Skye Contractor Inc. All rights reserved.&nbsp;</span>
+		<span>
+			Created by
+			<a href="https://github.com/junlee0325/sangkyu-kim" target="_blank">
+				Jun Kyung Lee
+			</a>
+		</span>
+	</footer>
     </div>
   );
 }
